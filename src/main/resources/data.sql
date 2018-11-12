2aab62bd3df429641bd20ed35b6345d969275965
rahul619302 <rahul.ab.singh@capgemini.com>
2018-11-12 13:36:35 +0530

    data sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO Person_Tbl (Person_Id, Person_Name, Person_Type) VALUES (1, 'Rahul Singh', 'employee');
+INSERT INTO Person_Tbl (Person_Id, Person_Name, Person_Type) VALUES (2, 'Rohan Singh', 'student');
